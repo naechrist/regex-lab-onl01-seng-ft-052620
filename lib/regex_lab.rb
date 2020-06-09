58704a8301985ea481579e9e7a28e072836a03db
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-09 16:57:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,11 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 #else 
 #  false 
 #  end
-  
+  if text.match(/^[A-Z].*\.$/) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
